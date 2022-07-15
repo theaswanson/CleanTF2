e77802e3ba52abf49205a5972752136591c77d32
Adam Swanson <adam.swanson.98@gmail.com>
2022-07-14 20:19:57 -0400

    Add option for upscaling textures

--- src/CleanTF2.CLI/Commands/FlattenCommand.cs
+++ src/CleanTF2.CLI/Commands/FlattenCommand.cs
@@ -39,6 +39,10 @@ namespace CleanTF2.CLI.Commands
             [CommandOption("--output-type")]
             [DefaultValue(1)]
             public int OutputType { get; set; }
+
+            [Description("Upscales flattened textures to 1024x1024. Helps address compatibility problems with sv_pure. (warning: takes more file size and processing time)")]
+            [CommandOption("--upscale")]
+            public bool Upscale { get; set; }
         }
 
         public override async Task<int> ExecuteAsync(CommandContext context, Settings settings)
@@ -52,15 +56,17 @@ namespace CleanTF2.CLI.Commands
             var hl2MaterialsSaveDirectory = Path.Combine(tempSaveDirectory, "hl2");
 
             // Flatten textures
-            await GenerateFlatTF2Textures(settings.TF2Directory, tf2MaterialsSaveDirectory);
-            await GenerateFlatHL2Textures(settings.TF2Directory, hl2MaterialsSaveDirectory);
+            await GenerateFlatTF2Textures(settings.TF2Directory, tf2MaterialsSaveDirectory, settings.Upscale);
+            await GenerateFlatHL2Textures(settings.TF2Directory, hl2MaterialsSaveDirectory, settings.Upscale);
 
             // Consolidate all textures
+            AnsiConsole.WriteLine("Preparing textures...");
             var saveMaterialsTo = Path.Combine(_directory.GetCurrentDirectory(), "flat-textures");
             _directory.CopyDirectory(tf2MaterialsSaveDirectory, saveMaterialsTo, recursive: true, overwrite: true);
             _directory.CopyDirectory(hl2MaterialsSaveDirectory, saveMaterialsTo, recursive: true, overwrite: true);
 
             // Delete temp directory of textures
+            AnsiConsole.WriteLine("Removing temp textures...");
             _directory.Delete(tempSaveDirectory, recursive: true);
 
             if (settings.OutputType == 1)
@@ -107,13 +113,13 @@ namespace CleanTF2.CLI.Commands
             }
         }
 
-        private async Task GenerateFlatTextures(string package, IEnumerable<string> materials, string saveTo)
+        private async Task GenerateFlatTextures(string package, IEnumerable<string> materials, string saveTo, bool upscale)
         {
             await AnsiConsole.Status().StartAsync("Starting", async context =>
             {
                 try
                 {
-                    await _flatTextureGenerator.Generate(package, materials, saveTo, upscale: true, (string status) => context.Status(status));
+                    await _flatTextureGenerator.Generate(package, materials, saveTo, upscale, (string status) => context.Status(status));
                 }
                 catch (Exception ex)
                 {
@@ -123,13 +129,13 @@ namespace CleanTF2.CLI.Commands
             });
         }
 
-        private async Task GenerateFlatTF2Textures(string tf2Directory, string saveTo)
+        private async Task GenerateFlatTF2Textures(string tf2Directory, string saveTo, bool upscale)
         {
             var tf2TextureFile = Path.Combine(tf2Directory, "tf", "tf2_textures_dir.vpk");
             var tf2Materials = await GetTF2MaterialList();
 
             AnsiConsole.WriteLine("Generating flat TF2 textures...");
-            await GenerateFlatTextures(tf2TextureFile, tf2Materials, saveTo);
+            await GenerateFlatTextures(tf2TextureFile, tf2Materials, saveTo, upscale);
         }
 
         private async Task<IEnumerable<string>> GetTF2MaterialList()
@@ -137,13 +143,13 @@ namespace CleanTF2.CLI.Commands
             return await _file.ReadAllLinesAsync("flat.txt");
         }
 
-        private async Task GenerateFlatHL2Textures(string tf2Directory, string saveTo)
+        private async Task GenerateFlatHL2Textures(string tf2Directory, string saveTo, bool upscale)
         {
             var hl2TextureFile = Path.Combine(tf2Directory, "hl2", "hl2_textures_dir.vpk");
             var hl2Materials = await GetHL2MaterialList();
 
             AnsiConsole.WriteLine("Generating flat HL2 textures...");
-            await GenerateFlatTextures(hl2TextureFile, hl2Materials, saveTo);
+            await GenerateFlatTextures(hl2TextureFile, hl2Materials, saveTo, upscale);
         }
 
         private async Task<IEnumerable<string>> GetHL2MaterialList()
